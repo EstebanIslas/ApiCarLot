f213ea303068d6c69e659d278589e4c28f316c54
STEPHAN <1717110261@utectulancingo.edu.mx>
2019-12-09 21:27:16 -0600

    Cambios en los procd

--- src/routes/parks.js
+++ src/routes/parks.js
@@ -20,18 +20,32 @@ router.get('/getParks', (req, res)=>{
 router.post('/registroParks', (req, res)=>{
     const{id, nombre, apellido, telefono, correo, password, rol, nombre_park, calle, 
         colonia, numero_ext, stock, dia_ini, dia_fin, hora_apertura, hora_cierre, 
-        descripcion, id_person} = req.body;
+        descripcion, tarifa, id_person} = req.body;
 
-    const query = `Call registroParks(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);`;
+    const query = `Call registroParks(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);`;
 
     pool.query(query, [id, nombre, apellido, telefono, correo, password, rol, nombre_park, calle, 
         colonia, numero_ext, stock, dia_ini, dia_fin, hora_apertura, hora_cierre, 
-        descripcion, id_person], (err, rows, fields)=>{
+        descripcion, tarifa, id_person], (err, rows, fields)=>{
         if (!err) {
             res.json({Status: 'Park Registrado'})
         } else {
             res.json({Status: 'Error!'});
             console.log('Error al Registrar Park', err);
+        }
+    });
+});
+
+router.post('/getOnePark', (req, res)=>{
+    //:nombre_park/:calle
+    const {id} = req.body;
+    //console.log(nombre_park, " " , calle);*/
+
+    pool.query('SELECT * FROM parks WHERE id = ?',[id], (err, rows, fields)=>{
+        if(!err){
+            res.json(rows);
+        }else{
+            console.log('Server has been Problems: ', err);
         }
     });
 });
